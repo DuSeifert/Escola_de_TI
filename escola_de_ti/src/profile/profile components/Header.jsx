beb33bd2323f8c0600db75b2e3f7c5b5e1a05ef3
DuSeifert <eduardoseifertes@gmail.com>
2026-04-06 20:49:39 -0300

    Alterações na tela de perfil

--- escola_de_ti/src/profile/profile components/Header.jsx
+++ escola_de_ti/src/profile/profile components/Header.jsx
@@ -1,9 +1,17 @@
+
+import Bannerimg from '../../assets/Banner.jpg'
+
 export default function Header(){
     return(
 
         <div className="header">
-            <div className="banner"/>
-
+            <div className="banner">
+                <img 
+                    src = {Bannerimg} 
+                    alt = ""
+                />
+            </div>
+                
             <div className="profile-info">
 
                 <div className="avatar">👤</div>
