fac1ece06b4f4ef75b87e87a3ed75e6423c241cf
MDrozino <migueldrozino882@gmail.com>
2026-04-06 21:43:53 -0300

    Criação do "BacklogCreation"

--- escola_de_ti/src/main.jsx
+++ escola_de_ti/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import BacklogCreation from './backlogcreation/BacklogCreation.jsx'
+
+
 import Profile from './profile/Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Profile/>
-
+    {/* <Profile/> */}
+  <BacklogCreation/>
 
   </StrictMode>,
 )
